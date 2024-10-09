c0ebc7d95057ff71ccd5cc65afa722aefec9a1b6
Kabillan T A <kabillan1905@gmail.com>
2024-10-10 03:22:16 +0530

    Oct 10 Progress

--- 10-10.py
+++ 10-10.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
 
 ##Dataset
 dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
@@ -39,3 +41,25 @@ for fearure_name in NUMERIC_COLUMNS:
 
 print(feature_columns)
 
+#training is done in batches 
+#epoch is the number of times the model will see the same data
+
+#Input function 
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+        if shuffle:
+            ds = ds.shuffle(1000)
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds
+    return input_function
+
+#linera classifier
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+
+#train the model 
+linear_est.train(make_input_fn(dftrain, y_train))
+result = linear_est.evaluate(make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False))
+clear_output()
+print(result['accuracy'])
